48acbcae161ad7f805b7640354391b7325b4acfb
Allan Farias <allan.digitaltec@gmail.com>
2022-04-13 09:37:13 -0300

    Add angular and Bootstrap

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './views/home/home.component';
+import { LoginComponent } from './views/login/login.component';
+import { AboutComponent } from './views/about/about.component';
+import { GamesComponent } from './views/games/games.component';
+import { HardwareComponent } from './views/hardware/hardware.component';
+import { PcGamerComponent } from './views/pc-gamer/pc-gamer.component';
+import { RegisterComponent } from './views/register/register.component';
+
+@NgModule({
+  declarations: [AppComponent, HomeComponent, LoginComponent, AboutComponent, GamesComponent, HardwareComponent, PcGamerComponent, RegisterComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
